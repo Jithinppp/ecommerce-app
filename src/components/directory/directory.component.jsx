4d6c0b20219301e9ce7790ace9c37dc5f65cc0f5
unknown <jithinp968@gmail.com>
2021-10-05 16:11:26 +0530

    broken homepage to smaller components & styled out the homepage

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.style.scss";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          id: 1,
+          title: "Hats",
+          imageUrl:
+            "http://images.unsplash.com/photo-1556306535-0f09a537f0a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+        },
+        {
+          id: 2,
+          title: "Sneakers",
+          imageUrl:
+            "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
+        },
+        {
+          id: 3,
+          title: "Jackets",
+          imageUrl:
+            "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1936&q=80",
+        },
+        {
+          id: 4,
+          title: "Womens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1581404917879-53e19259fdda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
+        },
+        {
+          id: 4,
+          title: "Mens",
+          imageUrl:
+            "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1148&q=80",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, id, imageUrl }) => (
+          <MenuItem title={title} key={id} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
+export default Directory;
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./menu-item.style.scss";
+
+function MenuItem({ title, imageUrl }) {
+  return (
+    <div style={{ backgroundImage: `url(${imageUrl})` }} className="menu-item">
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">shop now</span>
+      </div>
+    </div>
+  );
+}
+
+export default MenuItem;
