ad33dedb972d3eed903e671c196fd3e4791bdf79
unknown <jithinp968@gmail.com>
2021-10-06 19:44:28 +0530

    added the shop,collection-preview,collection-item components and styled

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
-<<<<<<< HEAD
 import Homepage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
-=======
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
->>>>>>> parent of dce41af (added dynamic routing to the menu-items using withRouter)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-<<<<<<< HEAD
         <Route exact path="/shop" component={ShopPage} />
-=======
-        <Route path="/hats" component={HatsPage} />
->>>>>>> parent of dce41af (added dynamic routing to the menu-items using withRouter)
       </Switch>
     </div>
   );
