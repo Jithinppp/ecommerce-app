dce41af2f3a96610bf916bb4268b756996bcd9aa
unknown <jithinp968@gmail.com>
2021-10-06 11:54:22 +0530

    added dynamic routing to the menu-items using withRouter

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage.component.jsx";
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
+const HatsPage = () => {
+  return <div>Hatspage</div>;
+};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,30 +11,35 @@ class Directory extends React.Component {
         {
           id: 1,
           title: "Hats",
+          links: "hats",
           imageUrl:
             "http://images.unsplash.com/photo-1556306535-0f09a537f0a3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
         },
         {
           id: 2,
           title: "Sneakers",
+          links: "sneakers",
           imageUrl:
             "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80",
         },
         {
           id: 3,
           title: "Jackets",
+          links: "jackets",
           imageUrl:
             "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1936&q=80",
         },
         {
           id: 4,
           title: "Womens",
+          links: "womens",
           imageUrl:
             "https://images.unsplash.com/photo-1581404917879-53e19259fdda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80",
         },
         {
           id: 5,
           title: "Mens",
+          links: "mens",
           imageUrl:
             "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1148&q=80",
         },
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, id, imageUrl }) => (
-          <MenuItem title={title} key={id} imageUrl={imageUrl} />
+        {this.state.sections.map(({ id, ...OtherSectionProps }) => (
+          <MenuItem key={id} {...OtherSectionProps} />
         ))}
       </div>
     );
